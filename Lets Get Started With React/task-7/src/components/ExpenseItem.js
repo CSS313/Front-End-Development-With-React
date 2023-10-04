cb391fd86c7da07b8237a2624e65f53cacf9a8a1
Chetan Singh <singh.chetan.313@gmail.com>
2023-10-04 17:41:24 +0530

    add code for task-7

---
+++ Lets Get Started With React/task-7/src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h2>Food</h2>
+        <h2>Rs 10</h2>
+      </div>
+      <div>
+        <h2>Petrol</h2>
+        <h2>Rs 100</h2>
+      </div>
+      <div>
+        <h2>Movies</h2>
+        <h2>Rs 200</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
